fbdfdffad522723fff56cf533755217b864e9bbf
SimatovRL <rusqwe@yandex.ru>
2023-05-30 11:09:04 +0300

    homework

---
+++ src/components/CardProduct.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="product-item">
+    <div class="product-img">
+      <img :src="props.product.image" />
+    </div>
+    <div class="product-list">
+      <h3>{{ props.product.title }}</h3>
+      <span>{{ props.product.description }}</span>
+      <span class="price">{{ props.product.price }}</span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps(["product"]);
+</script>
+<style scoped>
+.product-item {
+  width: 300px;
+  margin: 0 auto;
+  padding: 10px 10px 5px 10px;
+  border: 1px solid #eee;
+  background: white;
+  font-family: "Open Sans";
+  overflow: hidden;
+  transition: 0.4s linear;
+}
+.product-img {
+  transition: 1s ease-in-out;
+}
+.product-img:hover {
+  transform: scale(1.1);
+}
+.product-img img {
+  display: block;
+  width: 100%;
+}
+.product-list {
+  margin-top: 15px;
+}
+.product-list h3 {
+  font-weight: 700;
+  color: #39404b;
+  margin: 0;
+  text-transform: uppercase;
+  font-size: 14px;
+  line-height: 2;
+}
+.price {
+  color: #e34d38;
+  display: block;
+  margin-bottom: 12px;
+}
+.price {
+  color: #e34d38;
+  display: block;
+  margin-bottom: 12px;
+}
+</style>
---
+++ src/views/MainView.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    <CardProduct v-for="item of products" :product="item" :key="item.id" />
+  </main>
+</template>
+
+<script setup>
+import CardProduct from "../components/CardProduct.vue";
+import axios from "axios";
+import { ref, onMounted } from "vue";
+
+let products = ref([]);
+
+onMounted(() => getProducts());
+
+function getProducts() {
+  return axios.get("https://fakestoreapi.com/products", {}).then((res) => {
+    console.log(res.data);
+    products = res.data;
+  });
+}
+</script>
