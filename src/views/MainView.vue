2f672ffc4843e121cab6f3b19a73536054d18ef5
SimatovRL <rusqwe@yandex.ru>
2023-06-02 09:46:07 +0300

    Improved

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,45 +1,48 @@
 <template>
   <div>
     <button @click="isShowDialog = true">Сделать заказ</button>
-    <CardProduct v-for="item of products" :product="item" :key="item.id" />
-    <Dialog v-model="isShowDialog" @hide="hideDialog">
-      <template v-slot:header> Заказ </template>
-      <template v-slot:content>
-        <form @submit.prevent="submitForm">
-          <div class="row">
-            <label for="lastName">Фамилия:</label>
-            <input v-model="lastName" name="lastName" required />
-            <span class="error">{{ lastNameEr }}</span>
-          </div>
-          <div class="row">
-            <label for="firstName">Имя:</label>
-            <input type="text" name="firstName" v-model="firstName" required />
-            <span class="error">{{ firstNameEr }}</span>
-          </div>
-          <div class="row">
-            <label for="surname">Отчество:</label>
-            <input type="text" name="surname" v-model="surname" />
-          </div>
-          <div class="row">
-            <label for="address">Адрес:</label>
-            <input type="text" name="address" v-model="address" />
-            <span class="error">{{ addressEr }}</span>
-          </div>
-          <div class="row">
-            <label for="card">Номер карты:</label>
-            <input type="number" name="card" required v-model="card" />
-            <span class="error">{{ cardEr }}</span>
-          </div>
-          <div class="row">
-            <label for="expiry">Срок действия:</label>
-            <input type="number" name="expiry" required v-model="expiry" />
-            <span class="error">{{ expiryEr }}</span>
-          </div>
-          <input type="submit" />
-        </form>
-      </template>
-      <template v-slot:footer> </template>
-    </Dialog>
+    <p>
+      <label v-show="isShowConfirm">Форма успешно отправлена</label>
+      <CardProduct v-for="item of products" :product="item" :key="item.id" />
+      <Dialog v-model="isShowDialog" @hide="hideDialog">
+        <template v-slot:header> Заказ </template>
+        <template v-slot:content>
+          <Form @submit="submitForm">
+            <div class="row">
+              <label for="lastName">Фамилия:</label>
+              <Field name="lastName" :rules="validateText" v-model="order.lastName" />
+              <ErrorMessage class="error" name="lastName" />
+            </div>
+            <div class="row">
+              <label for="firstName">Имя:</label>
+              <Field name="firstName" :rules="validateText" v-model="order.firstName" />
+              <ErrorMessage class="error" name="firstName" />
+            </div>
+            <div class="row">
+              <label for="surname">Отчество:</label>
+              <input type="text" name="surname" v-model="order.surname" />
+            </div>
+            <div class="row">
+              <label for="address">Адрес:</label>
+              <Field name="address" :rules="validateText" v-model="order.address" />
+              <ErrorMessage class="error" name="address" />
+            </div>
+            <div class="row">
+              <label for="card">Номер карты:</label>
+              <Field name="card" :rules="validateCard" v-model="order.card" />
+              <ErrorMessage class="error" name="card" />
+            </div>
+            <div class="row">
+              <label for="expiry">Срок действия:</label>
+              <Field name="expiry" :rules="validateText" v-model="order.expiry" />
+              <ErrorMessage class="error" name="expiry" />
+            </div>
+            <input type="submit" />
+          </Form>
+        </template>
+        <template v-slot:footer> </template>
+      </Dialog>
+    </p>
   </div>
 </template>
 
@@ -48,13 +51,13 @@ import CardProduct from "../components/CardProduct.vue";
 import Dialog from "../components/Dialog.vue";
 import axios from "axios";
 import { ref, onMounted } from "vue";
-import { useField, useForm } from "vee-validate";
+import { useField, useForm, Field, Form, ErrorMessage } from "vee-validate";
 
 const products = ref([]);
 const order = ref({});
 const isShowDialog = ref(false);
+const isShowConfirm = ref(false);
 const surname = ref();
-const { handleSubmit } = useForm();
 
 onMounted(() => getProducts());
 
@@ -64,27 +67,24 @@ function getProducts() {
   });
 }
 function hideDialog() {
-  console.log("hideDialog");
+  //
 }
-function submitForm(values) {
-  console.log("submitForm");
-  isShowDialog.value = false;
+function clearForm() {
+  order.value = {};
 }
-const { value: lastName, errorMessage: lastNameEr } = useField("lastName", validateText);
-const { value: firstName, errorMessage: firstNameEr } = useField(
-  "firstName",
-  validateText
-);
-const { value: address, errorMessage: addressEr } = useField("address", validateText);
-const { value: card, errorMessage: cardEr } = useField("card", validateCard);
-const { value: expiry, errorMessage: expiryEr } = useField("expiry", validateText);
-
+function submitForm() {
+  return axios.post(" http://httpbin.org/post", order.value).then((res) => {
+    isShowDialog.value = false;
+    isShowConfirm.value = true;
+    clearForm();
+  });
+}
 
 function validateText(value) {
   if (!value) {
     return "Поле обязательно для заполнения";
   }
-  if (value.length < 2) {
+  if (value.length < 3) {
     return "Минимальное количество символов";
   }
   return true;
@@ -94,7 +94,7 @@ function validateCard(value) {
     return "Поле обязательно для заполнения";
   }
   if (String(value).length < 16) {
-    return "Введите валдиный номер карты";
+    return "Введите валидиный номер карты";
   }
   return true;
 }
